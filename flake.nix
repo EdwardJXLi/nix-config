f9e07e3f5e582713099af2fdc8c571ab88df8c4a
Edward Li <jiaxi.edwardli@gmail.com>
2026-04-23 22:32:43 -0700

    Add nixos-master back as an input

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-master.url = "github:nixos/nixpkgs/master";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
